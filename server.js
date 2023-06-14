8a1bdd9faab76f65a1e17ddc74413d8bbc5b1417
unknown <ahassan2001@msn.com>
2023-06-14 15:16:19 -0400

    created backend server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/recipesDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('Connected to MongoDB database');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
